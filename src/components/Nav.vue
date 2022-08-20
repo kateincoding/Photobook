ba9201a06298c53307ede5ef6d47c1d1e31a62e1
kateincoding <kateincoding@gmail.com>
2022-08-20 14:38:27 -0500

    [main]first schema finished

---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            async logout(){
+                await this.$store.dispatch("auth/logout");
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+// import AmplifyVue from '@aws-amplify/ui-vue';
+// import {
+//   applyPolyfills,
+//   defineCustomElements,
+// } from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(awsconfig);
+
+// applyPolyfills().then(() => {
+//   defineCustomElements(window);
+// });
+
 Vue.config.productionTip = false
 
 new Vue({
